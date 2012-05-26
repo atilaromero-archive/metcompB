e8f2c34d6fbc03814c0beef3bba5f4050837ce14
AtilaRomero <atilaromero@gmail.com>
2012-05-25 23:48:58 -0300

    renamed:    ex04/prog.py -> 04-euler-cromer/prog.py 	modified:   functions.py

--- functions.py
+++ functions.py
@@ -3,24 +3,50 @@
 def a(x):
     return -x
 
-class Metodos():
-    def __init__(self,x,v):
-        self.x=x
-        self.v=v
-    def next_euler(self,dt):
-        self.xold=self.x
-        v=self.v
-        self.v=self.v+a(x)*dt
-        self.x=self.x+v*dt
-        return self.x,self.v
-    def next_euler_cromer(self,dt):
-        self.xold=self.x
-        self.v=self.v+a(x)*dt
-        self.x=self.x+self.v*dt 
-        return self.x,self.v
-    def next_verlet(self,dt):
-        xnew=2*self.x-self.xold+a(self.x)*dt*dt
-        self.v=(xnew-self.xold)/(2*dt)
-        xold=self.x
-        self.x=xnew
-        return self.xold,self.v
+def nextv(v,x,gamma=0,dt):
+    g1=1/(1+gamma*dt/2)
+    g2=1-gamma*dt/2
+    v=g1*(g2*v+a(x)*dt/2)
+    return v
+
+def euler(x,v,dt):
+    vtemp=v
+    v=v+a(x)*dt
+    x=x+vtemp*dt
+    return x,v
+def euler_cromer(x,v,dt):
+    v=v+a(x)*dt
+    x=x+v*dt 
+    return x,v
+def verlet(xold,x,v,dt):
+    xnew=2*x-xold+a(x)*dt*dt
+    v=(xnew-xold)/(2*dt)
+    return x,v,xnew
+def leapfrog(x,v,dt):
+    vt=v
+    x=x+v*dt 
+    v=v+a(x)*dt
+    v=(vt+v)/2
+    return x,v
+def rungekutta4(x,v,dt):
+    a0=a(x)
+    x1=x+v*dt/2
+    v1=v+a0*dt/2
+    a1=a(x1)
+    x2=x+v1*dt/2
+    v2=v+a1*dt/2
+    a2=a(x2)
+    x3=x+v2*dt
+    v3=v+a2*dt
+    a3=a(x3)
+    x=x+(1.0/6)*(v+2*v1+2*v2+v3)*dt
+    v=v+(1.0/6)*(a0+2*a1+2*a2+a3)*dt
+    return x,v
+def rungekutta2(x,v,dt):
+    a0=a(x)
+    x1=x+v*dt/2
+    v1=v+a0*x*dt/2
+    a1=a(x1)
+    x=x+v1*dt
+    v=v+a1*x1*dt
+    return x,y
